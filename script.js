6a9c0c325d1de834cefb8b288cff9e0218bc2e4f
Pablo Souza <pablo.souza@take.net>
2024-08-22 10:03:51 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,13 @@
+const appKey = process.env.APP_KEY;
+
+(function () {
+        window.onload = function () {
+            new BlipChat()
+                .withAppKey(appKey)
+                .withButton({"color":"#0096fa","icon":""})
+                .withCustomCommonUrl('https://protos-take-nick.chat.blip.ai/')
+                .withTarget('main-container')
+                .build();
+        }
+})();
+            
